b79f9b752e1a147bb88197e204708f03d530b43a
Ishrak Adit <ishrak.adit07@gmail.com>
2024-05-01 02:47:02 +0600

    incomplete text

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,5 +1,5 @@
-import React from 'react';
-import { motion, reverseEasing, useScroll } from "framer-motion"
+import React, { useEffect, useRef } from 'react';
+import { motion, reverseEasing, useAnimation, useInView, useScroll, useTransform } from "framer-motion"
 
 const gridContainerVariants = {
     hidden: {opacity: 0}, 
@@ -32,6 +32,26 @@ const svgIconVariants = {
 const Home = () => {
 
     const {scrollYProgress:completionProgress} = useScroll();
+
+    const containerRef = useRef(null);
+
+    const isInView = useInView(containerRef, {once: true})
+    const mainControls = useAnimation();
+
+    const {scrollYProgress} = useScroll({
+        target: containerRef,
+        offset: ["start end", "end end"],
+    })
+
+    c
+    const paragraphOneValue = useTransform(scrollYProgress, [0, 1], ["-100%", "0%"]);
+    const paragraphTwoValue = useTransform(scrollYProgress, [0, 1], ["100%", "0%"]);
+
+    useEffect(()=>{
+        if(isInView){
+            mainControls.start("visible");
+        }
+    }, [isInView])
 
   return (
     <div className='flex flex-col gap-10 overflow-x-hidden'>
@@ -152,6 +172,47 @@ const Home = () => {
             </motion.svg>
 
         </motion.div>
+
+        {/*First Text*/}
+        <section className='flex flex-col gap-10 mb-10' ref={containerRef}>
+            <motion.h1 
+            className='text-5xl tracking-wide text-slate-900 text-center'
+            animate={mainControls}
+            initial="hidden"
+            variants={{
+                hidden: {
+                    opacity: 0, y:75
+                },
+                visible: {
+                    opacity: 1, y:0
+                }
+            }}
+            transition={{delay:0.3}}
+            >Bayern 2 - 1 Real Madrid
+            </motion.h1>
+        </section>
+
+        <motion.p className='text-slate-900 font-thin text-4xl w-1/2 mx-auto'>
+            In the dim light of dawn, Katniss Everdeen stood at the edge of the forest, 
+            her bow poised, senses sharp. The haunting echoes of the Hunger Games arena 
+            whispered through the trees, a reminder of the brutality she had endured. Yet, 
+            amidst the chaos, a flicker of hope ignited within her, fueled by memories of 
+            Rue's gentle spirit and Peeta's unwavering strength. With each arrow loosed, 
+            she defied the Capitol's tyranny, a symbol of resistance for the oppressed districts. 
+            As the sun climbed higher, casting shadows across the arena, Katniss vowed to keep fighting, 
+            for freedom, for justice, for a future where the odds were finally in their favor.
+        </motion.p>
+
+        <motion.p className='text-slate-900 font-thin text-4xl w-1/2 mx-auto'>
+        In the hallowed halls of the Louvre, Robert Langdon stood, deciphering cryptic symbols 
+        that danced across ancient manuscripts. The air hummed with secrets, whispers of a truth 
+        buried beneath layers of history and deception. With each clue unraveled, he delved deeper 
+        into the enigma of the Holy Grail, pursued by shadows that lurked in the shadows. Alongside 
+        Sophie Neveu, their quest became a race against time, navigating a labyrinth of hidden codes 
+        and religious intrigue. As they unearthed the mysteries of the past, Langdon realized that 
+        the greatest revelation was not in the search for answers, but in the journey of enlightenment 
+        itself.
+        </motion.p>
 
       </motion.section>
     </div>
