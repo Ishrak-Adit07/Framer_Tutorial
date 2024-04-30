90c6c840dcba9f46c527c2344bdd8f2fdb116e88
Ishrak Adit <ishrak.adit07@gmail.com>
2024-05-01 02:04:02 +0600

    Fourth square commit

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -80,12 +80,33 @@ const Home = () => {
         {/*Fourth Square*/}
         <motion.div 
         variants={gridSquareVariants}
-        className='bg-slate-800 aspect-square rounded-lg justify-center flex items-center gap-10'></motion.div>
+        className='bg-slate-800 aspect-square rounded-lg justify-center flex items-center gap-10'>
+
+            <motion.div className='w-1/3 h-1/3 bg-orange-500 rounded-3xl cursor-grab' 
+            drag
+            dragConstraints={{
+                top: -125, 
+                right: 125, 
+                left: 125,
+                bottom: -125
+            }}
+            dragTransition={{
+                bounceStiffness: 600, bounceDamping:10
+            }}>
+            </motion.div>
+
+        </motion.div>
 
         {/*Fifth Square*/}
         <motion.div 
         variants={gridSquareVariants}
-        className='bg-slate-800 aspect-square rounded-lg justify-center flex items-center gap-10'></motion.div>
+        className='bg-slate-800 aspect-square rounded-lg justify-center flex items-center gap-10'>
+
+            <motion.div className='w-40 aspect-square bg-gray-50/20 rounded-xl'>
+
+            </motion.div>
+
+        </motion.div>
 
         {/*Sixth Square*/}
         <motion.div 
