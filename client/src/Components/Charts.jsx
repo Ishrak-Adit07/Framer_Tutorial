a64d496fad5da55bfaa7a0c5cc01d896634a487f
Ishrak Adit <ishrak.adit07@gmail.com>
2024-07-15 14:07:38 +0600

    Recharts biaxial line chart commit

--- client/src/Components/Charts.jsx
+++ client/src/Components/Charts.jsx
@@ -1,12 +1,14 @@
 /* eslint-disable no-unused-vars */
 import React from "react";
 import StraightAnglePieChart from "./Charts/StraightAnglePieChart";
+import BiaxialLineChart from "./Charts/BiaxialLineChart";
 
 const Charts = () => {
   return (
     <div className="flex flex-wrap justify-center items-center mt-10">
       <h1 className="text-4xl">This is Charts Section</h1>
       <StraightAnglePieChart />
+      <BiaxialLineChart />
     </div>
   );
 };
